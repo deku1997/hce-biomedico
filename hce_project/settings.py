7b0e25238e113d91e49a7bb866c5b7066bb4ba2e
TU_USUARIO_GITHUB <TU_CORREO@ejemplo.com>
2026-08-26 09:44:53 -0400

    Fix: ejecutar migraciones antes de iniciar en Railway

--- hce_project/settings.py
+++ hce_project/settings.py
@@ -75,3 +75,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # 🔥 CONFIGURACIÓN PARA ARCHIVOS SUBIDOS (IMÁGENES)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
